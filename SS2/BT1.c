10a55047b2db1699229d7a5763634d93f7662ea9
dungphamrk <147359437+dungphamrk@users.noreply.github.com>
2023-11-07 21:24:31 +0700

    Add files via upload

---
+++ SS2/BT1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    const PI =3.14;
+    int canhHV = 10;
+    int chieuDai= 20, chieuRong=15;
+    int banKinh =10;
+    int chuViHV,chuViHcn, chuViHT, dienTichHcn,dienTichHT,dienTichHV;
+    chuViHcn= (chieuDai+chieuRong)*2;
+    chuViHV= canhHV*4;
+    chuViHT= banKinh*2*PI;
+    dienTichHcn=chieuDai*chieuRong;
+    dienTichHV=canhHV*canhHV;
+    dienTichHT=banKinh*banKinh*PI;
+}
